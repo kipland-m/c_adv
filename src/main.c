67833d1520f6fc2a7f3c9d51c19614c499679668
Kipland Melton <kiplandmelton@gmail.com>
2024-06-19 10:03:42 -0500

    restructuring dir

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool parseAndExecute(char *input, int lightLevel)
+// main input function- game loop will run until this function returns false
+bool parseAndExecute(char *input)
 {
    char *verb = strtok(input, " \n");
 
@@ -17,15 +18,7 @@ bool parseAndExecute(char *input, int lightLevel)
       // feel
       else if (strcmp(verb, "feel") == 0)
       {
-         printf("Feel where?\n");
-      }
-      else if (strcmp(verb, "feel") == 0)
-      {
-         printf("Feel where?\n");
-      }
-      else if (strcmp(verb, "feel") == 0)
-      {
-         printf("Feel where?\n");
+         printf("You feel around and find a lighter at your feet.\n");
       }
 
       
@@ -35,7 +28,7 @@ bool parseAndExecute(char *input, int lightLevel)
       {
          printf("It is very dark in here.\n");
       }
-      else if (strcmp(verb, "look") == 0 && lightLevel == 1)
+      else if (strcmp(verb, "look") == 0 && lightLevel >= 1)
       {
          printf("The room is small and made of stone. The ceiling is very low and there is a door.\n");
       }
@@ -45,6 +38,13 @@ bool parseAndExecute(char *input, int lightLevel)
       else if (strcmp(verb, "go") == 0)
       {
          printf("It's too dark to go anywhere.\n");
+      }
+
+      // area specific?
+      // action
+      else if (strcmp(verb, "light") == 0)
+      {
+	printf("You light the lighter.");
       }
 
 
@@ -68,10 +68,19 @@ int main(){
 	// GAME VARIABLES
 	int lightLevel = 0;
 	int playerHealth = 0;
-	
+
+	FILE* fptr;
+
+	fptr = fopen("inv.txt","w");
+
+	if (fptr != NULL)
+	{
+		printf("inventory file created successfully!\n\n");
+	}
+
 	printf("Welcome to the game\n");
 
-	while (parseAndExecute(input, lightLevel) && getInput());
+	while (parseAndExecute(input) && getInput());
 
 	printf("\nBye!\n");
 	return 0;
